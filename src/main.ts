2224ec8541dd84949c6578751a43b8c28f629e88
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2023-02-06 17:19:10 -0800

    add executable

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,21 @@ import { exec } from '@actions/exec'
 async function run(): Promise<void> {
   try {
     await exec('touca', ['version'])
-    const inputs: Partial<Record<'version' | 'directory', string>> =
-      Object.fromEntries(
-        ['version', 'directory']
-          .map((k) => [k, getInput(k)])
-          .filter(([_, v]) => v != '')
-      )
-    const args = []
+    const inputs: Partial<
+      Record<'version' | 'directory' | 'executable', string>
+    > = Object.fromEntries(
+      ['version', 'directory', 'executable']
+        .map((k) => [k, getInput(k)])
+        .filter(([_, v]) => v != '')
+    )
+    const args = inputs.executable ? [inputs.executable] : ['touca', 'test']
     if (inputs.version) {
-      args.push(`--revision=${inputs.version}`)
+      args.push('--revision', inputs.version)
     }
-    if (inputs.directory) {
-      args.push(`--testdir=${inputs.directory}`)
+    if (!inputs.executable && inputs.directory) {
+      args.push('--testdir', inputs.directory)
     }
-    await exec('touca', ['test', ...args])
+    await exec(args[0], args.slice(1))
   } catch (error) {
     if (error instanceof Error) {
       setFailed(error.message)
