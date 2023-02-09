c5901d3ccb6b2df5657bd78ed9f7a81e629fcdbf
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2023-02-09 15:20:08 -0800

    build: add basic logic for adding summary

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,48 @@
 // Copyright 2023 Touca, Inc. Subject to Apache-2.0 License.
 
-import { getInput, setFailed } from '@actions/core'
+import { getInput, setFailed, summary } from '@actions/core'
 import { exec } from '@actions/exec'
+
+type Report = {
+  suite: string
+  version: string
+  rows: Array<Record<'status' | 'name' | 'time', string>>
+  link: string
+}
+
+function parseReports(output: string): Array<Report> {
+  return [
+    {
+      suite: 'Students',
+      version: 'v1.0',
+      rows: [
+        { status: 'SENT', name: 'alice', time: '251 ms' },
+        { status: 'SENT', name: 'bob', time: '245 ms' },
+        { status: 'SENT', name: 'charlie', time: '220 ms' }
+      ],
+      link: 'http://localhost:4200/~/acme/students_2/v1.0'
+    }
+  ]
+}
+
+async function printReport(report: Report) {
+  await summary
+    .addHeading('Comparison Results for {}/{}', 2)
+    .addTable([
+      ['', 'Status', 'Name', 'Runtime'].map((v) => ({
+        data: v,
+        header: true
+      })),
+      ...report.rows.map((v, i) => [
+        (i + 1).toFixed(0),
+        v.status,
+        v.name,
+        v.time
+      ])
+    ])
+    .addLink('View comparison results on the Touca server', report.link)
+    .write()
+}
 
 async function runExe() {
   const args = [getInput('executable')]
@@ -21,7 +62,21 @@ async function runCli() {
   if (getInput('directory') !== '') {
     args.push('--testdir', getInput('directory'))
   }
-  await exec(args[0], args.slice(1))
+  const stream = { out: '', err: '' }
+  await exec(args[0], args.slice(1), {
+    listeners: {
+      stdout: (data: Buffer) => {
+        stream.out += data.toString()
+      },
+      stderr: (data: Buffer) => {
+        stream.err += data.toString()
+      }
+    }
+  })
+  await summary.addHeading('Regression Test Results').write()
+  for (const report of parseReports(stream.out)) {
+    printReport(report)
+  }
 }
 
 try {
